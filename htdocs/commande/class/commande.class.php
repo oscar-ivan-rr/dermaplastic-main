0e358d1e4a340032f40d217aa9f6cb2683c98f86
Oscar Ramirez <oscarramirez@MacBook-Air-de-Oscar.local>
2025-11-28 20:32:04 -0600

    Fix backend url for print labels function

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -4471,7 +4471,7 @@ class Commande extends CommonOrder
         $url = '';
         switch ($marketplace) {
             case 'ML':
-                $url = 'http://backend:8080/api/mercadolibre/orders/'.$this->pack_id.'/shipping_label';
+                $url = BACKEND_URL . 'mercadolibre/orders/'.$this->pack_id.'/shipping_label';
                 break;
             default:
                 break;
