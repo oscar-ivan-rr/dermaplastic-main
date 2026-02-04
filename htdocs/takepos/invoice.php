6aa80642d91779bf993052846391e1675d35cd85
Oscar Ramirez <oscarramirez@MacBook-Air-de-Oscar.local>
2026-02-04 15:08:42 -0600

    Disable update disc

--- htdocs/takepos/invoice.php
+++ htdocs/takepos/invoice.php
@@ -1139,15 +1139,15 @@ $applyDiscontLines2 = array();
 $DISCOUNT = 15;
 
 //ORIGINAL DISCOUNTS
-foreach ($invoice->lines as $line) {
-	$prod = new Product($db);
-	$prod->fetch($line->fk_product);
-	$customer = new Societe($db);
-	$customer->fetch($invoice->socid);
-	$descuento = ($prod->temp_discount != 0) ? $prod->temp_discount : (($prod->desc_max != 0) ? (($prod->desc_max >= 10) ? $customer->remise_percent : $prod->desc_max) : 0);
-	$invoice->updateline($line->id, $line->desc, $line->subprice, $line->qty, $descuento, $line->date_start, $line->date_end, $line->tva_tx, $line->localtax1_tx, $line->localtax2_tx, 'HT', $line->info_bits, $line->product_type, $line->fk_parent_line, 0, $line->fk_fournprice, $line->pa_ht, $line->label, $line->special_code, $line->array_options, $line->situation_percent, $line->fk_unit);
-	$invoice->fetch($placeid);
-}
+// foreach ($invoice->lines as $line) {
+// 	$prod = new Product($db);
+// 	$prod->fetch($line->fk_product);
+// 	$customer = new Societe($db);
+// 	$customer->fetch($invoice->socid);
+// 	$descuento = ($prod->temp_discount != 0) ? $prod->temp_discount : (($prod->desc_max != 0) ? (($prod->desc_max >= 10) ? $customer->remise_percent : $prod->desc_max) : 0);
+// 	$invoice->updateline($line->id, $line->desc, $line->subprice, $line->qty, $descuento, $line->date_start, $line->date_end, $line->tva_tx, $line->localtax1_tx, $line->localtax2_tx, 'HT', $line->info_bits, $line->product_type, $line->fk_parent_line, 0, $line->fk_fournprice, $line->pa_ht, $line->label, $line->special_code, $line->array_options, $line->situation_percent, $line->fk_unit);
+// 	$invoice->fetch($placeid);
+// }
 
 if($category1Found && $category2Found) {
 	//APPLY CUSTOM DISCOUNT
