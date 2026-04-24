50655044d20e868adf665ac2eb3373bf064b1fc3
Oscar Ramirez <oscarramirez@MacBook-Air-de-Oscar.local>
2026-04-23 22:55:39 -0600

    Fix product import

--- htdocs/custom/import/class/product.template.class.php
+++ htdocs/custom/import/class/product.template.class.php
@@ -423,7 +423,7 @@ class ProductTemplate extends Template {
                 $rescat = $this->product->AddProductCategory($catid->rowid);
             }
         }
-        ShopifyPrice($this->product->id);
+        updateShopifyPrice($this->product->id);
 
         unset($entrepot);
         unset($this->product);
